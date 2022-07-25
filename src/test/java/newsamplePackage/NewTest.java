78b4f4d9e1c2cd21036217104356152ea08515b6
akhtar0806 <akansariitm120@gmail.com>
2022-07-25 15:04:00 +0530

    do changes

---
+++ src/test/java/newsamplePackage/NewTest.java
@@ -0,0 +1,13 @@
+package newsamplePackage;
+
+public class NewTest {
+
+	public static void main(String[] args) {
+		//print statement for new gitbash
+		System.out.println("new gitbash");
+		//ptint second time
+		System.out.println("news gitbash");	
+		System.out.println("hi");
+	}
+
+}
